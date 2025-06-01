a34395551cb415d12e0f53fe42de8c5956f0b163
Sudhanva <sudhanvapops@gmail.com>
2025-06-01 11:36:59 +0530

    Better Version

--- 00_Ceaser_Cypher/CeaserCypher.java
+++ 00_Ceaser_Cypher/CeaserCypher.java
@@ -6,25 +6,23 @@ public class CeaserCypher {
         StringBuilder result = new StringBuilder();
 
         Scanner sc = new Scanner(System.in);
-        
-        
-        System.out.println("Enter the String To Cypher: ");
+
+        System.out.println("\n\nEnter the String To Cypher: ");
         String userString = sc.nextLine();
-        
+
         System.out.print("Enter the number of shift want to Perform: ");
         int shift = sc.nextInt();
         shift = shift % 26;
 
-
         for (char c : userString.toCharArray()) {
 
-            if (c >= 'a' && c<= 'z'){
-                char shifted = (char)('a' + (c - 'a' + shift) % 26);
+            if (c >= 'a' && c <= 'z') {
+                char shifted = (char) ('a' + (c - 'a' + shift) % 26);
                 result.append(shifted);
-            } else if(c>='A' && c <= 'Z'){
-                char shifted = (char)('A'+ (c - 'A' + shift) % 26);
+            } else if (c >= 'A' && c <= 'Z') {
+                char shifted = (char) ('A' + (c - 'A' + shift) % 26);
                 result.append(shifted);
-            } else{
+            } else {
                 result.append(c);
             }
 
@@ -35,12 +33,55 @@ public class CeaserCypher {
         return result.toString();
     }
 
+
+    public static void display(){
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            System.out.println("Error clearing screen: " + e.getMessage());
+        }
+
+        System.out.println("\n=======================");
+        System.out.println("Ceaser Cypher");
+        System.out.println("""
+                  _____
+                 /     \\
+                | () () |
+                 \\  ^  /
+                  |||||
+                  |||||
+                """);
+        System.out.println("=======================");
+        System.out.println("1. Cypher\n2. Decypher");
+        System.out.println("=======================");
+    }
+
     public static void main(String[] args) {
 
-        String cypherString = Cypher();
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Cyphered Text: " + cypherString);
+        display();
 
+        System.out.print("Enter the Choice: ");
+        int choice = sc.nextInt();
+
+        if (choice == 1) {
+
+            String cypherString = Cypher();
+            System.out.println("\nCyphered Text: " + cypherString+"\n\n");
+
+        } else if (choice == 2) {
+            System.out.println("Please Stand By.......");
+
+        } else {
+            System.out.println("Invalid Choice");
+        }
+
+        sc.close();
     }
 
 }
