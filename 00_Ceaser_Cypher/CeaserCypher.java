ef5bf3c1f4b9c97c1cc92dd26649c8c42b4d91b0
Sudhanva <sudhanvapops@gmail.com>
2025-06-01 11:25:43 +0530

    Improved Cypher Function

--- 00_Ceaser_Cypher/CeaserCypher.java
+++ 00_Ceaser_Cypher/CeaserCypher.java
@@ -2,46 +2,42 @@ import java.util.Scanner;
 
 public class CeaserCypher {
 
-    public static String Cypher(char[] alphabets) {
+    public static String Cypher() {
+        StringBuilder result = new StringBuilder();
 
         Scanner sc = new Scanner(System.in);
-
-        String cypherString = "";
-
+        
+        
         System.out.println("Enter the String To Cypher: ");
         String userString = sc.nextLine();
-
+        
         System.out.print("Enter the number of shift want to Perform: ");
         int shift = sc.nextInt();
+        shift = shift % 26;
 
-        userString = userString.toLowerCase();
-        char[] userStringArray = userString.toCharArray();
-        for (char c : userStringArray) {
 
-            for (int i = 0; i <= alphabets.length - 1; i++) {
-                if (c == alphabets[i]) {
-                    cypherString += alphabets[i + shift];
-                    break;
-                }
+        for (char c : userString.toCharArray()) {
+
+            if (c >= 'a' && c<= 'z'){
+                char shifted = (char)('a' + (c - 'a' + shift) % 26);
+                result.append(shifted);
+            } else if(c>='A' && c <= 'Z'){
+                char shifted = (char)('A'+ (c - 'A' + shift) % 26);
+                result.append(shifted);
+            } else{
+                result.append(c);
             }
 
         }
 
         sc.close();
 
-        return cypherString;
+        return result.toString();
     }
 
     public static void main(String[] args) {
 
-        char[] alphabets = {
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-        };
-
-        String cypherString = Cypher(alphabets);
+        String cypherString = Cypher();
 
         System.out.println("Cyphered Text: " + cypherString);
 
