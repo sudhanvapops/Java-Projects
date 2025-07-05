c7d4979ca51090578917e894d4e0378123342423
Sudhanva <sudhanvapops@gmail.com>
2025-07-06 00:51:06 +0530

    made validation method and added validation to constructors and setters

--- 01_Campus_Managment_System/model/Book.java
+++ 01_Campus_Managment_System/model/Book.java
@@ -1,49 +1,54 @@
 package model;
+
 public class Book {
-    
+
     private String bookId;
     private String title;
     private String author;
 
+    // Getters
     public String getBookId() {
         return bookId;
-    }
-
-    public void setBookId(String bookId) {
-        this.bookId = bookId;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    // Setters
+    public void setBookId(String bookId) {
+        this.bookId =  validateNotEmpty(bookId, "Book ID");
     }
 
+    public void setTitle(String title) {
+        this.title = validateNotEmpty(title, "Title");
+    }
+
+    public void setAuthor(String author) {
+        this.author = validateNotEmpty(author, "Author");
+    }
+
+
+
+    // Constructor
     public Book(String bookId, String title, String author) {
-        this.bookId = bookId;
-        this.title = title;
-        this.author = author;
+        this.bookId = validateNotEmpty(bookId, "Book ID");
+        this.title = validateNotEmpty(title, "Title");
+        this.author = validateNotEmpty(author, "Author");
     }
 
     // Validation Method
-    private String validateNotEmpty(String value,String fieldname){
+    private String validateNotEmpty(String value, String fieldname) {
         value = value.trim();
-        if(value == null || value.isEmpty()){
-            throw new IllegalArgumentException(fieldname+" cannot be empty");
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException(fieldname + " cannot be empty");
         }
         return value;
     }
-
 
     @Override
     public String toString() {
